813a97a7c6faf337515c49a5a24702383cdd48f2
Ankesh Anand <ankeshanand1994@gmail.com>
2015-09-28 02:38:06 +0530

    Add BST functions

--- Abstract-Data-Structures/binary_search_trees.cpp
+++ Abstract-Data-Structures/binary_search_trees.cpp
@@ -18,10 +18,88 @@ class BST{
         ~BST(){
             destroy_tree();
         }
-        void insert(int key);
-        node* search(int key);
+
+        Node* search(const int & key){
+            search(key, root);
+        }
+
+        void insert(const int & key){
+            insert(key, root);
+        }
+
+        void remove(const int & key){
+            remove(key, root);
+        }
+
+        const int & findMin(){
+            Node* min_node = findMin(root);
+            if(min_node != NULL)
+                return min_node->data;
+            return -1;
+        }
+
         void destroy_tree();
     
     private:
-        Node *root;       
+        Node *root;
+        Node* search(const int &key, Node* & t);        
+        void insert(const int & key, Node* & t);
+        Node* findMin(Node *t);
+        void remove(const int & key, Node* & t);
 };
+
+Node* BST::search(const int & key, Node* & t){
+    if(t == NULL)
+        return NULL;
+
+    if(key == t->data)
+        return t;
+
+    if(key < t->data)
+        return search(key, t->left);
+
+    return search(key, t->right);
+}
+
+void BST::insert(const int & key, Node * & t){
+    if(t == NULL){
+        t = new Node(key);
+    }
+    else if(key < t->data)
+        insert(key, t->left);
+    else if(key > t->data)
+        insert(key, t->right);
+    else
+        ; //duplicate: do nothing
+}
+
+void BST::remove(const int & key, Node * & t){
+    if(t == NULL)
+        return; // key not found
+
+    if(key < t->data)
+        remove(key, t->left);
+
+    if(key > t->data)
+        remove(key, t->right);
+
+    else if(t->left != NULL && t->right != NULL){
+        Node* min_right = findMin(t->right);
+        t->data = min_right->data;
+        remove(min_right->data, t->right);
+    }
+
+    else{
+        Node* old_node = t;
+        t = (t->left != NULL) ? t->left : t->right;
+        delete old_node;
+    }
+}
+
+Node* BST::findMin(Node *t){
+    if(t != NULL){
+        while(t->left != NULL)
+            t = t->left;
+    }
+    return t;
+}
